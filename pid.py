2836f78d96b98c7981d2104f677319306fdb1b4c
Casey Meisenzahl <casey.meisenzahl@gmail.com>
2024-11-25 15:42:32 -0500

    .

---
+++ pid.py
@@ -0,0 +1,20 @@
+import gymnasium as gym
+
+env=gym.make('CartPole-v1',render_mode='human')
+(state,_)=env.reset()
+
+initial_state = env.reset()
+env.render()
+action = env.action_space.sample()
+pole_e = [0]
+cart_e = [0]
+for i in range(1, 1000):
+    observation, reward, terminated, truncated, info = env.step(action)
+    cp, cv, pa, pv = observation
+    pole_e.append(0 - pa)
+    cart_e.append(0 - cp)
+    p = (5 * pole_e[-1]) + (30 * (pole_e[-1] - pole_e[-2]))
+    c = (1 * cart_e[-1]) + (20 * (cart_e[-1] - cart_e[-2]))
+
+    action = 1 if p + c < 0 else 0
+
---
+++ simple.py
@@ -0,0 +1,16 @@
+import gymnasium as gym
+import time
+
+env=gym.make('CartPole-v1',render_mode='human')
+(state,_)=env.reset()
+
+
+initial_state = env.reset()
+env.render()
+action = env.action_space.sample()
+for i in range(1000):
+    observation, reward, terminated, truncated, info = env.step(action)
+    cp, cv, pa, pv = observation
+    action = 1 if pa > 0 else 0
+    time.sleep(0.05)
+
